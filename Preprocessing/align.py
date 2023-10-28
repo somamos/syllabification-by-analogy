1c413b09bd74974bc0ce2a2ae3d0675fe7548779
Mark Ramos <somamosinteractive@protonmail.com>
2023-10-28 07:46:00 -0700

    Change dict to list mapping (allow duplicates).

--- Preprocessing/align.py
+++ Preprocessing/align.py
@@ -2,9 +2,7 @@
 #       for Speech Technology Applications Using an EM-Like Algorithm"
 
 class Aligner:
-
 	class Matrix:
-
 		class Cell:
 			def iterate(self):
 				self.val += 1
@@ -14,28 +12,24 @@ class Aligner:
 				self.val = val
 
 		def get(self, i, j):
-			return self.A[ self.L[i] ][ self.P[j] ].val
+			return self.A[ self.L.index(i) ][ self.P.index(j) ].val
 
 		def set(self, i, j, val):
-			self.A[ self.L[i] ][ self.P[j] ] = val
+			self.A[ self.L.index(i) ][ self.P.index(j) ] = val
 
 		def iterate(self, i, j):
-			self.A[ self.L[i] ][ self.P[j] ].iterate()
+			self.A[ self.L.index(i) ][ self.P.index(j) ].iterate()
 
 		def __init__(self, rows, cols, init = None):
 			# Map all phonemes and letters to indices.
-			self.L = {}
-			self.P = {}
-			for index, letter in enumerate(rows):
-				self.L[letter] = index
-			for index, phoneme in enumerate(cols):
-				self.P[phoneme] = index
+			self.L = rows
+			self.P = cols
 			self.A = [] # Populate association matrix with initial values.
-			for i in self.L.keys():
+			for i in self.L:
 				row = []
-				for j in self.P.keys():
+				for j in self.P:
 					val = 0 if init is None else init.get(i, j)
-					row.append(self.Cell(i, j, 0))
+					row.append(self.Cell(i, j, val))
 				self.A.append(row)
 
 		def __str__(self):
@@ -50,14 +44,17 @@ class Aligner:
 	# Letters will map to row indices -- Phonemes, column indices -- of a
 	# so-called association matrix. It scores the likelihood of pairings.
 	def __init__(self, alphabet, phonemes, wordlist):
+		self.LETTER_PAD = '#'
+		self.PHONEME_PAD = '$'
+		
 		# Given two lists of characters and an optional matrix of starting values;
 		# Return the matrix, A, as well as two dicts, L and P,
 		# which map single characters to indices in A.
 
 
 		# Add characters to represent the borders of words.
-		alphabet.append('#')
-		phonemes.append('$')
+		alphabet.append(self.LETTER_PAD)
+		phonemes.append(self.PHONEME_PAD)
 		# A's values will be counts.
 		A = self.Matrix(alphabet, phonemes)
 
@@ -72,12 +69,39 @@ class Aligner:
 
 		# Begin alignment.
 		def score(word):
-			# B's values, initial scores.
-			B = self.Matrix(word.letters, word.phonemes, A)
-			# C's values will be accumulated scores.
-			C = self.Matrix(word.letters, word.phonemes)
-			# D's values will point to previous Cells.
-			D = self.Matrix(word.letters, word.phonemes)
+			# Pad.
+			padded_letters = self.LETTER_PAD + word.letters + self.LETTER_PAD
+			padded_phones = self.PHONEME_PAD + word.phonemes + self.PHONEME_PAD
+
+			# B's values will be initial scores.
+			B = self.Matrix(padded_letters, padded_phones, A)
+			print(B)
+			# C's values, accumulated scores.
+			C = self.Matrix(padded_letters, padded_phones)
+			# D's values, references to "previous Cells."
+			D = self.Matrix(padded_letters, padded_phones)
+
+		print('Printing the word {}'.format(wordlist[0].letters))
+		for i in range (100):
+			score(wordlist[i])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
