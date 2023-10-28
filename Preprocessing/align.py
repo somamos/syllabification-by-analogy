89c2107d42ed48934d3a79c4206e3a117247bc72
Mark Ramos <somamosinteractive@protonmail.com>
2023-10-28 08:51:49 -0700

    Word scoring function

--- Preprocessing/align.py
+++ Preprocessing/align.py
@@ -4,21 +4,25 @@
 class Aligner:
 	class Matrix:
 		class Cell:
-			def iterate(self):
-				self.val += 1
 			def __init__(self, letter, phoneme, val):
 				self.letter = letter
 				self.phoneme = phoneme
 				self.val = val
 
-		def get(self, i, j):
-			return self.A[ self.L.index(i) ][ self.P.index(j) ].val
 
-		def set(self, i, j, val):
-			self.A[ self.L.index(i) ][ self.P.index(j) ] = val
+		def get_by_index(self, i, j):
+			return self.A[ i ][ j ].val
+		def set_by_index(self, i, j, val):
+			self.A[ i ][ j ].val = val
+		def iterate_by_index(self, i, j):
+			self.A[ i ][ j ].iterate()
 
-		def iterate(self, i, j):
-			self.A[ self.L.index(i) ][ self.P.index(j) ].iterate()
+		def get(self, l, p):
+			return self.A[ self.L.index(l) ][ self.P.index(p) ].val
+		def set(self, l, p, val):
+			self.A[ self.L.index(l) ][ self.P.index(p) ] = val
+		def iterate(self, l, p):
+			self.A[ self.L.index(l) ][ self.P.index(p) ].val += 1
 
 		def __init__(self, rows, cols, init = None):
 			# Map all phonemes and letters to indices.
@@ -33,10 +37,13 @@ class Aligner:
 				self.A.append(row)
 
 		def __str__(self):
-			s = ''
-			for row in self.A:
+			s = ' '
+			s += ''.join(['{:>8}'.format(p) for p in self.P]) # Print col labels.
+			s += '\n'
+			for i, row in enumerate(self.A):
+				s += '{}'.format(self.L[i]) # Print row labels.
 				for cell in row:
-					s += '{}{}: {}, '.format(cell.letter, cell.phoneme, cell.val)
+					s += '{:>8}'.format(cell.val) # Print individual cells.
 				s += '\n'
 			return s
 
@@ -76,10 +83,40 @@ class Aligner:
 			# B's values will be initial scores.
 			B = self.Matrix(padded_letters, padded_phones, A)
 			print(B)
+			print('\n\n')
 			# C's values, accumulated scores.
 			C = self.Matrix(padded_letters, padded_phones)
 			# D's values, references to "previous Cells."
 			D = self.Matrix(padded_letters, padded_phones)
+			# Iterate through C, adding max of prev vals and pointing D towards it.
+			for i, ch in enumerate(padded_letters):
+				for j, ph in enumerate(padded_phones):
+					# Beginning cases.
+					if i == 0 or j == 0:
+						continue
+					maximum = 0
+					curr = 0
+					curr_coord = None
+					# Either cell above,
+					o1 = C.get_by_index(i - 1, j)
+					# cell to the left,
+					o2 = C.get_by_index(i, j - 1)
+					# or cell to the top left plus current.
+					o3 = C.get_by_index(i - 1, j - 1) + B.get_by_index(i, j)
+					
+					best = max((o1, o2, o3))
+
+					C.set_by_index(i, j, best)
+					if best == o3:
+						D.set_by_index(i, j, '\\')
+					elif best == o2:
+						D.set_by_index(i, j, '->')
+					elif best == o1:
+						D.set_by_index(i, j, ('|'))
+			print(C)
+			print('\n\n')
+			print(D)
+			print('\n\n\n\n\n')
 
 		print('Printing the word {}'.format(wordlist[0].letters))
 		for i in range (100):
